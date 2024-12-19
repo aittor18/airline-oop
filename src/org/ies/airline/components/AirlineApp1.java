d8eb80f7b7d179760483c35b7efddd374b3255a7
aittor18 <rodriguezaitor678@gmail.com>
2024-12-19 11:09:35 +0100

    AirlineApp1 terminado :)

--- src/org/ies/airline/components/AirlineApp1.java
+++ src/org/ies/airline/components/AirlineApp1.java
@@ -1,6 +1,8 @@
 package org.ies.airline.components;
 
 import org.ies.airline.model.Airline;
+import org.ies.airline.model.Flight;
+import org.ies.airline.model.Passenger;
 
 import java.util.Scanner;
 
@@ -19,7 +21,40 @@ public class AirlineApp1 {
 
         //Pide al usuario un número de vuelo
         System.out.println("Introduce un número de vuelo: ");
+        int flightNumber = scanner.nextInt();
+        scanner.nextLine();
 
+        //Busca el vuelo con ese número de vuelo
+        Flight flight = airline.findFlight(flightNumber);
+
+        // Si no existe muestra por pantalla un error
+        if (flight == null) {
+            System.out.println("ERROR!, El número de vuelo o existe...");
+        }
+        //Si existe:
+        else {
+            //Pide al usuario un pasajero.
+            System.out.print("Introduce el NIF del pasajero: ");
+            String nif = scanner.nextLine();
+
+            //Busca a ese pasajero dentro del vuelo:
+            Passenger passenger = flight.findPassenger(nif);
+
+            // Si existe:
+            if (passenger != null) {
+                //Muestra los datos del pasajeroç
+                passenger.showInfo();
+
+                //Pide un número de asiento
+                System.out.println("Introduce un número de asiento: ");
+                int seatNumber = scanner.nextInt();
+                scanner.nextLine();
+
+                //Cambia el asiento del pasajero
+
+            }
+
+        }
 
     }
 }
