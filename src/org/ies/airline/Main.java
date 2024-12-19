9c0805216df715920afe0e2986dc8aee6c1146dd
aittor18 <rodriguezaitor678@gmail.com>
2024-12-19 11:02:43 +0100

    Empezando de nuevo

--- src/org/ies/airline/Main.java
+++ src/org/ies/airline/Main.java
@@ -1,9 +1,11 @@
 package org.ies.airline;
 
+import org.ies.airline.components.AirlineApp1;
 import org.ies.airline.components.AirlineReader;
 import org.ies.airline.components.FlightReader;
 import org.ies.airline.components.PassengerReader;
 import org.ies.airline.model.Airline;
+import org.ies.airline.model.Passenger;
 
 import java.util.Scanner;
 
@@ -13,10 +15,13 @@ public class Main {
         var passengerReader = new PassengerReader(scanner);
         var flightReader = new FlightReader(scanner, passengerReader);
         var airlineReader = new AirlineReader(scanner, flightReader);
+        var airlineApp1 = new AirlineApp1(scanner, airlineReader, passengerReader)
 
-        Airline airline = airlineReader.read();
+        //Airline airline = airlineReader.read();
 
-        System.out.println(airline);
+        //System.out.println(airline);
+
+
 
     }
 }
